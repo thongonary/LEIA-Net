a563666b03927bdfa11edbedf297f4aec7961f77
Thong <thongnguyen1292@gmail.com>
2019-10-10 03:13:14 -0700

    First commit

---
+++ interaction.py
@@ -0,0 +1,130 @@
+"""
+Tensorflow implementation of the Interaction networks for the identification of boosted Higgs to bb decays https://arxiv.org/abs/1909.12285 
+"""
+
+import os
+import itertools
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import numpy as np
+
+class InteractionModel(models.Model):
+    def __init__(self, n_constituents, n_targets, params, hidden, fr_activation=0, fo_activation=0, fc_activation=0, sum_O=True):
+        super(InteractionModel, self).__init__()
+        self.hidden = int(hidden)
+        self.P = params
+        self.N = n_constituents
+        self.Nr = self.N * (self.N - 1)
+        self.Dr = 0
+        self.De = 8
+        self.Dx = 0
+        self.Do = 8
+        self.n_targets = n_targets
+        self.fr_activation = fr_activation
+        self.fo_activation = fo_activation
+        self.fc_activation = fc_activation 
+        self.assign_matrices()
+        self.Ra = tf.ones([self.Dr, self.Nr])
+        self.fr1 = layers.Dense(self.hidden, input_shape=(2 * self.P + self.Dr,))
+        self.fr2 = layers.Dense(int(self.hidden/2), input_shape=(self.hidden,))
+        self.fr3 = layers.Dense(self.De, input_shape=(int(self.hidden/2),))
+        
+        self.fo1 = layers.Dense(self.hidden, input_shape=(self.P + self.Dx + (2 * self.De),))
+        self.fo2 = layers.Dense(int(self.hidden/2), input_shape=(self.hidden,))
+        self.fo3 = layers.Dense(self.Do, input_shape=(int(self.hidden/2),))
+        
+        self.fc1 = layers.Dense(hidden)
+        self.fc2 = layers.Dense(int(hidden/2))
+        self.fc3 = layers.Dense(self.n_targets)
+        self.sum_O = sum_O 
+    
+    def assign_matrices(self):
+        Rr = np.zeros([self.N, self.Nr], dtype=np.float32)
+        Rs = np.zeros([self.N, self.Nr], dtype=np.float32)
+        receiver_sender_list = [i for i in itertools.product(range(self.N), range(self.N)) if i[0]!=i[1]]
+        for i, (r, s) in enumerate(receiver_sender_list):
+            Rr[r, i]  = 1
+            Rs[s, i] = 1
+        self.Rr = tf.convert_to_tensor(Rr)
+        self.Rs = tf.convert_to_tensor(Rs)
+        del Rs, Rr
+
+    def call(self, x):
+        '''
+        Expect input to have shape of (batches, N_particles, N_features)
+        '''
+        ###PF Candidate - PF Candidate###
+        x = tf.transpose(x, perm=[0, 2, 1])
+        Orr = self.tmul(x, self.Rr)
+        Ors = self.tmul(x, self.Rs)
+        B = tf.concat([Orr, Ors], 1)
+        ### First MLP ###
+        B = tf.transpose(B, perm=[0, 2, 1])
+        if self.fr_activation == 2:
+            B = tf.nn.selu(self.fr1(tf.reshape(B, [-1, 2 * self.P + self.Dr])))
+            B = tf.nn.selu(self.fr2(B))
+            E = tf.nn.selu(tf.reshape(self.fr3(B), [-1, self.Nr, self.De]))
+        elif self.fr_activation == 1:
+            B = tf.nn.elu(self.fr1(tf.reshape(B, [-1, 2 * self.P + self.Dr])))
+            B = tf.nn.elu(self.fr2(B))
+            E = tf.nn.elu(tf.reshape(self.fr3(B), [-1, self.Nr, self.De]))
+        else:
+            B = tf.nn.relu(self.fr1(tf.reshape(B, [-1, 2 * self.P + self.Dr])))
+            B = tf.nn.relu(self.fr2(B))
+            E = tf.nn.relu(tf.reshape(self.fr3(B), [-1, self.Nr, self.De]))
+        del B
+        E = tf.transpose(E, perm=[0, 2, 1])
+        Ebar = self.tmul(E, tf.transpose(self.Rr, perm=[1, 0]))
+        del E
+       
+        ####Final output matrix for particles###
+        C = tf.concat([x, Ebar], 1)
+        del Ebar
+        C = tf.transpose(C, perm=[0, 2, 1])
+        print(f"C {C.shape}")
+        
+        ### Second MLP ###
+        if self.fo_activation == 2:
+            C = tf.nn.selu(self.fo1(tf.reshape(C, [-1, self.P + self.Dx + self.De])))
+            C = tf.nn.selu(self.fo2(C))
+            O = tf.nn.selu(tf.reshape(self.fo3(C), [-1, self.N, self.Do]))
+        elif self.fo_activation == 1:
+            C = tf.nn.elu(self.fo1(tf.reshape(C, [-1, self.P + self.Dx + self.De])))
+            C = tf.nn.elu(self.fo2(C))
+            O = tf.nn.elu(tf.reshape(self.fo3(C), [-1, self.N, self.Do]))
+        else:
+            C = tf.nn.relu(self.fo1(tf.reshape(C, [-1, self.P + self.Dx + self.De])))
+            C = tf.nn.relu(self.fo2(C))
+            O = tf.nn.relu(tf.reshape(self.fo3(C), [-1, self.N, self.Do]))
+        del C
+       
+        if self.sum_O:
+            O = tf.reduce_sum(O, 1)
+
+        ### Classification MLP ###
+        if self.fc_activation == 2:
+            if self.sum_O:
+                N = tf.nn.selu(self.fc1(tf.reshape(O, [-1, self.Do * 1])))
+            else:
+                N = tf.nn.selu(self.fc1(tf.reshape(O, [-1, self.Do * N])))
+            N = tf.nn.selu(self.fc2(N))   
+        if self.fc_activation == 1:
+            if self.sum_O:
+                N = tf.nn.elu(self.fc1(tf.reshape(O, [-1, self.Do * 1])))
+            else:
+                N = tf.nn.elu(self.fc1(tf.reshape(O, [-1, self.Do * N])))
+            N = tf.nn.elu(self.fc2(N)) 
+        else:
+            if self.sum_O:
+                N = tf.nn.relu(self.fc1(tf.reshape(O, [-1, self.Do * 1])))
+            else:
+                N = tf.nn.relu(self.fc1(tf.reshape(O, [-1, self.Do * N])))
+            N = tf.nn.relu(self.fc2(N))
+        print(f"N {N.shape}")
+        N = self.fc3(N)
+        return N
+
+    def tmul(self, x, y):  #Takes (I * J * K)(K * L) -> I * J * L 
+        x_shape = tf.shape(x)
+        y_shape = tf.shape(y)
+        return tf.reshape(tf.matmul(tf.reshape(x, [-1, x_shape[2]]), y), [-1, x_shape[1], y_shape[1]]) 
---
+++ training.py
@@ -0,0 +1,183 @@
+from __future__ import print_function
+import os
+import numpy as np
+import pandas as pd
+import imp
+import datetime
+try:
+    imp.find_module('setGPU')
+    import setGPU
+except ImportError:
+    pass    
+import glob
+import sys
+import tqdm
+import argparse
+import pathlib
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from interaction import InteractionModel
+
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+if os.path.isdir('/data/shared/hls-fml/'):
+    test_path = '/data/shared/hls-fml/NEWDATA/'
+    train_path = '/data/shared/hls-fml/NEWDATA/'
+elif os.path.isdir('/eos/project/d/dshep/hls-fml/'):
+    test_path = '/eos/project/d/dshep/hls-fml/'
+    train_path = '/eos/project/d/dshep/hls-fml/'
+
+N = 100 # number of particles
+n_targets = 5 # number of classes
+n_features = 16
+
+
+def main(args):
+    """ Main entry point of the app """
+    
+    from data import H5Data
+    files = glob.glob(train_path + "/jetImage*_{}*.h5".format(N))
+    files_val = files[4:5] # take first 5 for validation
+    files_train = files[5:6] # take rest for training
+    
+    label = 'new'
+
+    batch_size = 256
+    data_train = H5Data(batch_size = batch_size,
+                        cache = None,
+                        preloading=0,
+                        features_name='jetConstituentList', 
+                        labels_name='jets',
+                        spectators_name=None)
+    data_train.set_file_names(files_train)
+    data_val = H5Data(batch_size = batch_size,
+                      cache = None,
+                      preloading=0,
+                      features_name='jetConstituentList', 
+                      labels_name='jets',
+                      spectators_name=None)
+    data_val.set_file_names(files_val)
+
+    n_val=data_val.count_data()
+    n_train=data_train.count_data()
+
+    print("val data:", n_val)
+    print("train data:", n_train)
+
+    net_args = (N, n_targets, n_features, args.hidden)
+    net_kwargs = {"fr_activation": 0, "fo_activation": 0, "fc_activation": 0}
+    
+    gnn = InteractionModel(*net_args, **net_kwargs)
+    
+    #### Start training ####
+    
+    n_epochs = 1
+    # Keep results for plotting
+    train_loss_results = []
+    train_accuracy_results = []
+    val_loss_results = []
+    val_accuracy_results = []
+    
+    # Log directory for Tensorboard
+    current_time = datetime.datetime.now().strftime("%Y%m%d")
+    train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+    test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+    pathlib.Path(train_log_dir).mkdir(parents=True, exist_ok=True)  
+    pathlib.Path(test_log_dir).mkdir(parents=True, exist_ok=True)  
+
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+
+    for epoch in range(n_epochs):
+        
+        # Tool to keep track of the metrics
+        epoch_loss_avg = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
+        epoch_accuracy = tf.keras.metrics.CategoricalAccuracy('train_accuracy')
+        val_epoch_loss_avg = tf.keras.metrics.Mean('test_loss', dtype=tf.float32)
+        val_epoch_accuracy = tf.keras.metrics.CategoricalAccuracy('test_accuracy')
+
+        # Training
+        for sub_X, sub_Y in tqdm.tqdm(data_train.generate_data(),total = n_train/batch_size):
+#            print(f"sub_X: {sub_X.shape}")
+#            print(f"sub_Y: {sub_Y.shape}")
+            training = sub_X.astype(np.float32)
+            target = sub_Y.astype(np.float32)[:,-6:-1]
+            # Define loss function
+            cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+            def loss(model, x, y):
+                y_ = model(x)
+                return cce(y_true=y, y_pred=y_)
+            def grad(model, input_par, targets):
+                with tf.GradientTape() as tape:
+                    loss_value = loss(model, input_par, targets)
+                return loss_value, tape.gradient(loss_value, model.trainable_variables)
+            
+            # Define optimizer
+            optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+            # Compute loss and gradients
+            loss_value, grads = grad(gnn, training, target)
+
+            # Update the gradients
+            optimizer.apply_gradients(zip(grads, gnn.trainable_variables))
+
+            # Track progress
+            epoch_loss_avg(loss_value)  # Add current batch loss
+            # Compare predicted label to actual label
+            epoch_accuracy(target, tf.nn.softmax(gnn(training)))
+
+        # Validation
+        for sub_X, sub_Y in tqdm.tqdm(data_val.generate_data(),total = n_val/batch_size):
+            training = sub_X.astype(np.float32)
+            target = sub_Y.astype(np.float32)[:,-6:-1]
+            
+            
+            # Compute the loss
+            loss_value = loss(gnn, training, target)
+            
+            # Track progress
+            val_epoch_loss_avg(loss_value)
+            val_epoch_accuracy(target, tf.nn.softmax(gnn(training)))
+
+        # End epoch
+        train_loss_results.append(epoch_loss_avg.result())
+        train_accuracy_results.append(epoch_accuracy.result())
+        val_loss_results.append(val_epoch_loss_avg.result())
+        val_accuracy_results.append(val_epoch_accuracy.result())
+        
+        # Logs for tensorboard
+        with train_summary_writer.as_default():
+            tf.summary.scalar('loss', epoch_loss_avg.result(), step=epoch)
+            tf.summary.scalar('accuracy', epoch_accuracy.result(), step=epoch)
+        with test_summary_writer.as_default():
+            tf.summary.scalar('loss', val_epoch_loss_avg.result(), step=epoch)
+            tf.summary.scalar('accuracy', val_epoch_accuracy.result(), step=epoch)
+
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print (template.format(epoch+1,
+                         epoch_loss_avg.result(), 
+                         epoch_accuracy.result()*100,
+                         val_epoch_loss_avg.result(), 
+                         val_epoch_accuracy.result()*100))
+
+        # Reset metrics every epoch
+        epoch_loss_avg.reset_states()
+        val_epoch_loss_avg.reset_states()
+        epoch_accuracy.reset_states()
+        val_epoch_accuracy.reset_states()
+
+    # Save the model after training
+    save_path = 'models/1/'
+    pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)  
+    tf.saved_model.save(gnn, save_path)
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    
+    # Required positional arguments
+    
+    # Optional arguments
+    parser.add_argument("--hidden", type=int, action='store', dest='hidden', default = 15, help="hidden")
+
+    args = parser.parse_args()
+    main(args)
